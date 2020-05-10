4426290349d4d3c501e5d7fd945c9d900cf35082
Artem Fedotov <artemfedotov045@gmail.com>
2020-05-10 11:54:23 +0300

    s

---
+++ src/main/java/com/start/traviscidemo/TestController.java
@@ -0,0 +1,18 @@
+package com.start.traviscidemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String greeting() {
+        return "Hello, World";
+    }
+
+    @GetMapping("/hello2")
+    public String greeting2() {
+        return "Hello, World";
+    }
+}
